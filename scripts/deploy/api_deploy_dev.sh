2c153ae9b538ab75d67f3d8032bbc7e1e684465f
captnseagraves <k.s.seagraves@gmail.com>
2020-10-18 15:20:42 -0700

    Changed Fonts, began color changes

--- scripts/deploy/api_deploy_dev.sh
+++ scripts/deploy/api_deploy_dev.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
 
-function remove_two_newest_files()
-{
-  rm -rf "$(stat -c "%Y:%n" /backend/downtownapi/main/migrations/* | sort -t: -n | tail -1 | cut -d: -f2-)"
-  rm -f "$(stat -c "%Y:%n" /backend/downtownapi/main/migrations/* | sort -t: -n | tail -1 | cut -d: -f2-)"
-}
-
-trap remove_two_newest_files EXIT
-
 python /backend/downtownapi/manage.py makemigrations
 python /backend/downtownapi/manage.py migrate
 python /backend/downtownapi/manage.py loaddata /backend/downtownapi/sampledata.json
 python /backend/downtownapi/manage.py runserver 0.0.0.0:8000
-
-sleep infinity # We sleep now, on shutdown we call the trap to clean up the temporary database migration
