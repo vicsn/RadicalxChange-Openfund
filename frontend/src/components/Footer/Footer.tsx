4d6f5e97c048a770052ad08f7871ed69afc7c4e1
madeline3000 <test@gmail.com>
2020-10-28 13:37:04 +1100

    Implemented changes to footer and seed data

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -4,125 +4,80 @@ import { ActionContext } from "../../hooks";
 
 function Footer() {
   const { setModalConfig } = useContext(ActionContext);
-  const signupFormUrl = `${process.env.REACT_APP_SIGNUP_FORM_URL}`;
+  /*const signupFormUrl = `${process.env.REACT_APP_SIGNUP_FORM_URL}`;
   const requestFormUrl = `${process.env.REACT_APP_REQUEST_FORM_URL}`;
   const supportEmail = `${process.env.REACT_APP_SUPPORT_EMAIL}`;
   const twitterUrl = `${process.env.REACT_APP_TWITTER_URL}`;
-  const blogUrl = `${process.env.REACT_APP_BLOG_URL}`;
+  const blogUrl = `${process.env.REACT_APP_BLOG_URL}`;*/
 
   return (
     <div className="footer">
       <div className="footer-container-left container-spacing-set">
         <h3 className="top-margin-set">Get Involved</h3>
-        <p className="top-margin-set"> This is a test.</p>
-        <ul>
-          <li>
-            <a href={signupFormUrl} rel="noopener noreferrer" target="_blank">
-              Boulder Business Sign-Up Form
-            </a>
-          </li>
-          <li>
-            <a href={requestFormUrl} rel="noopener noreferrer" target="_blank">
-              Bring Downtown Stimulus to My Town
-            </a>
-          </li>
-        </ul>
-        <h3 className="top-margin-set">About Quadratic Funding</h3>
         <p className="top-margin-set">
           {" "}
-          Downtown Stimulus is a democratic way of funding projects. They’ve put
-          together a matching pool of $25k from local philanthropists that is going
-          to be distributed to downtown Boulder Businesses, to help bridge the gap
-          between normal and now. {/* eslint-disable-next-line */}
-          <a onClick={(e) => setModalConfig(true, { type: "qfExplainer" })}>
-            Read More
-          </a>
+          Interested in participating in the next RxC funding round? Want to
+          contribute to the matching pool of RxC Openfund? Want to bring Openfund to
+          your non-profit organization? Please Get in touch below.
         </p>
-
-        <h3 className="top-margin-set">Legal Info</h3>
-        <p className="top-margin-set bottom-margin-set">
+        <ul>
+          <li>
+            Want to contribute to the matching pool of RxC Openfund? Contact Matt
+            Prewitt - matt@radicalxchange.org
+          </li>
+          <li>
+            Bring Openfund to your non-profit organization. Contact Leon Erichsen -
+            leon@radicalxchange.org
+          </li>
+        </ul>
+        <h3 className="top-margin-set">About RxC Openfund</h3>
+        <p className="top-margin-set">
           {" "}
-          By accessing and using this website, you are acknowleding and agreeing to
-          Downtown Stimulus's {/* eslint-disable-next-line */}
-          <a onClick={(e) => setModalConfig(true, { type: "legalInfo" })}>
-            Terms & Conditions and Privacy Policy
-          </a>{" "}
+          RadicalxChange Foundation is a nonprofit organization. Our work is free and
+          public, to support the RxC movement. Almost all our activities rely on
+          contributions from our community and philanthropic organizations. RxC
+          Openfund uses Quadratice Funding (QF) to incentivize and govern funding for
+          RadicalxChange Foundation. It allows people to contribute to their favorite
+          RxC projects, e.g. conferences, fellowships, hackathons or podcasts. These
+          contributions are matched by a fund from RadicalxChange Foundation.{" "}
+          {/* eslint-disable-next-line */}
         </p>
       </div>
       <div className="footer-container-right container-spacing-set">
-        <h3 className="top-margin-set">Contact & Follow</h3>
+        <h3 className="top-margin-set">Legal Info</h3>
         <div className="footer-contact-container top-margin-set">
-          <span className="footer-contact-icon">
-            <a href={"mailto: ".concat(supportEmail)}>
-              <img src={require("../../assets/email.svg")} alt="mail" />
+          <p>
+            By accessing and using this website, you are acknowledging and agreeing
+            to RadicalxChange Foundation's {/* eslint-disable-next-line */}
+          <a onClick={(e) => setModalConfig(true, { type: "legalInfo" })}>
+              Terms & Conditions and Privacy Policy
             </a>
-          </span>
-          <span className="footer-contact-icon">
-            <a href={twitterUrl} target="_blank" rel="noopener noreferrer">
-              <img src={require("../../assets/tw.svg")} alt="twitter" />
-            </a>
-          </span>
-          <span className="footer-contact-icon">
-            <a href={blogUrl} target="_blank" rel="noopener noreferrer">
-              <img src={require("../../assets/medium.svg")} alt="medium" />
-            </a>
-          </span>
+          </p>
         </div>
-        <h3 className="top-margin-set">Partners</h3>
+        <h3 className="top-margin-set">Supporters</h3>
         <div className="top-margin-set bottom-margin-set">
           <div className="footer-sponsors-icon">
             <a
-              href={"https://gitcoin.co/"}
+              href={"https://www.radicalxchange.org/"}
               target="_blank"
               rel="noopener noreferrer"
             >
               <img
-                src={require("../../assets/image 23.png")}
-                alt="Gitcoin"
-                className="footer-partner-logo"
-              />
-            </a>
-          </div>
-          <div className="footer-sponsors-icon">
-            <a
-              href={"https://labs.consensys.net/"}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img
-                src={
-                  "https://labs.consensys.net/static/45c707c85b9d26f5c1eb2976856974a7/af144/consensys-labs-typeface-logo-white.png"
-                }
-                alt="ConsenSys Labs"
+                src={require("../../assets/image 23.png")} /*Kevin can you change this image to the RxC logo image in the footer folder*/
+                alt="RadicalxChange Foundation"
                 className="footer-partner-logo"
               />
             </a>
           </div>
         </div>
-        <h4 className="footer-support-header">
-          Pilot Funding proudly supported by:
-        </h4>
-        <ul>
-          <li>
-            <a href="https://feld.com" rel="noopener noreferrer" target="_blank">
-              Brad Feld
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://gitcoin.co/grants"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              GitCoin Grantees
-            </a>
-          </li>
-          <li>
-            <a href="https://owocki.com" rel="noopener noreferrer" target="_blank">
-              Kevin Owocki
-            </a>
-          </li>
-        </ul>
+        <h3 className="top-margin-set">RxC Openfund Codebase</h3>
+        <a href="https://feld.com" rel="noopener noreferrer" target="_blank">
+          <img
+            src={require("../../assets/image 23.png")} /*Kevin can you change this image to the Github logo image in the footer folder*/
+            alt="Github"
+            className="footer-partner-logo"
+          />
+        </a>
       </div>
     </div>
   );
