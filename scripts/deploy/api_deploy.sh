975d2795d5a5e1ab4dee07e1ebd90e33a3a3b5a2
vicsn <vicsn@users.noreply.github.com>
2020-10-29 22:11:19 +0000

    By default add initial CLR round in production

--- scripts/deploy/api_deploy.sh
+++ scripts/deploy/api_deploy.sh
@@ -1,4 +1,5 @@
 python /backend/quadraticfundingapi/manage.py makemigrations
 python /backend/quadraticfundingapi/manage.py migrate
+python /backend/quadraticfundingapi/manage.py loaddata /backend/quadraticfundingapi/productiondata.json
 python /backend/quadraticfundingapi/manage.py collectstatic --noinput
 gunicorn --workers=3 quadraticfundingapi.wsgi:application --bind 0.0.0.0:8000
