6e16eae4bac4f6d6baf351903ebdd0c6f6f8a25e
vicsn <vicsn@users.noreply.github.com>
2020-10-31 15:52:47 +0000

    Increase log level of backend

--- scripts/deploy/api_deploy.sh
+++ scripts/deploy/api_deploy.sh
@@ -2,4 +2,4 @@ python /backend/quadraticfundingapi/manage.py makemigrations
 python /backend/quadraticfundingapi/manage.py migrate
 python /backend/quadraticfundingapi/manage.py loaddata /backend/quadraticfundingapi/productiondata.json
 python /backend/quadraticfundingapi/manage.py collectstatic --noinput
-gunicorn --workers=3 quadraticfundingapi.wsgi:application --bind 0.0.0.0:8000
+gunicorn --workers=3 quadraticfundingapi.wsgi:application --bind 0.0.0.0:8000 --log-level info
